d9f462fc86195f329a654937f7807e49c30c48be
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-27 10:44:14 -0500

    Creación de todo list e implementación de useReducer Hook

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,13 @@ import { Padre } from './07-tarea-memo/Padre';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { HooksApp } from './HooksApp';
 
-import './08-useReducer/intro-reducer';
+// import './08-useReducer/intro-reducer';
+import { TodoApp } from './08-useReducer/TodoApp';
 
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <Padre />
-//   // </React.StrictMode>
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <TodoApp />
+  // </React.StrictMode>
+)
