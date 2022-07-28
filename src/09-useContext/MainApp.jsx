452f8a243444fa8facdd6d93fe361bcb0cad5d42
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-28 11:09:43 -0500

    Se crea un contexto el cual provee la información de un objeto en este caso

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -4,13 +4,14 @@ import {
     Navigate,
 } from 'react-router-dom';
 import { AboutPage } from './AboutPage';
+import { UserProvider } from './context/UserProvider';
 import { HomePage } from "./HomePage";
 import { LoginPage } from './LoginPage';
 import { Navbar } from './Navbar';
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             <h1>MainApp</h1>            
             <Navbar />
             <hr />
@@ -22,6 +23,6 @@ export const MainApp = () => {
 
                 <Route path="/*" element={<Navigate to="/about" />} />
             </Routes>
-        </>
+        </UserProvider>
     )
 }
