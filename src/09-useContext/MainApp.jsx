b311d208e79c097c6b015f205da8ed88d280f04c
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-28 10:26:59 -0500

    Configuración de rutas

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,25 @@
+import {
+    Routes,
+    Route,
+    Navigate
+} from 'react-router-dom';
+import { AboutPage } from './AboutPage';
+import { HomePage } from "./HomePage";
+import { LoginPage } from './LoginPage';
+
 export const MainApp = () => {
-  return (
-    <>
-        <h1>MainApp</h1>
-        <hr />
-    </>
-  )
+    return (
+        <>
+            <h1>MainApp</h1>
+            <hr />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="about" element={<AboutPage />} />
+                <Route path="login" element={<LoginPage />} />
+
+                <Route path="/*" element={<Navigate to="/about" />} />
+            </Routes>
+        </>
+    )
 }
