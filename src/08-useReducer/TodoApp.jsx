1f66cdd54ea8ee2d2306629429b012f0f759a3bf
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-28 09:27:19 -0500

    Se separa la lógica de TodoApp, se crea custom hook useTodos

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,61 +1,14 @@
-import { useEffect, useReducer } from "react";
+import { useTodos } from '../hooks';
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer";
-
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     todo: 'Recolectar flores',
-    //     done: false,
-    // },
-    // {
-    //     id: new Date().getTime() * 3,
-    //     todo: 'Recolectar fresas',
-    //     done: false,
-    // },
-];
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
-}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos])
-
-
-    const handleNewTodo = (todo) => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
-
-        dispatch(action);
-    }
-
-    const handleDeleteTodo = (id) => {
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        });
-    }
-
-    const handleToggleTodo = (id) => {
-        console.log({id});
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        });
-    }
+    const { todos, todosCount, pendingTodosCount, handleNewTodo, handleDeleteTodo, handleToggleTodo }  = useTodos();
 
     return (
         <>
-            <h1>TodoApp: 10, <small>pendientes: 2</small></h1>
+            <h1>TodoApp: { todosCount }, <small>pendientes: { pendingTodosCount }</small></h1>
             <hr />
 
             <div className="row">
