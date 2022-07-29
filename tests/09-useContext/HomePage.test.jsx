c69d7cedf00a238872d451b705b74d30f80ec782
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-29 16:06:31 -0500

    Pruebas con useContext

---
+++ tests/09-useContext/HomePage.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { UserContext } from '../../src/09-useContext/context/UserContext';
+import { HomePage } from '../../src/09-useContext/HomePage';
+
+describe('Pruebas en HomePage component', () => {
+
+    const user = {
+        id: 1,
+        name: 'Fernando'
+    }
+
+    test('debe de mostrar el componente sin el usuario', () => {
+
+        render(
+            <UserContext.Provider value={{ user: null }}>
+                <HomePage />
+            </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); // aria-label
+        expect(preTag.innerHTML).toBe('null');
+        // screen.debug()
+    });
+
+    test('debe de mostrar el componente con el usuario', () => {
+
+        render(
+            <UserContext.Provider value={{ user: user }}>
+                <HomePage />
+            </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); // aria-label
+        expect(preTag.innerHTML).toContain( user.name );
+        expect(preTag.innerHTML).toContain( `${user.id}` );
+        // screen.debug()
+
+    });
+
+});
