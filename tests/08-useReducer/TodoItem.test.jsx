a599b5cabbafde44d47550d37feec3ed78a41a60
Adriana Venegas <adrianne.venegas@gmail.com>
2022-07-29 15:13:37 -0500

    Pruebas en los eventos del TodoItem component

--- tests/08-useReducer/TodoItem.test.jsx
+++ tests/08-useReducer/TodoItem.test.jsx
@@ -33,4 +33,56 @@ describe('Pruebas en TodoItem', () => {
 
     });
 
+    test('debe mostrar el todo completado', () => {
+
+        todo.done = true;
+
+        render(
+            <TodoItem
+                todo={todo}
+                onDeleteTodo={onDeleteTodoMock}
+                onToggleTodo={onToggleTodoMock}
+            />
+        );
+        screen.debug();
+
+        const spanElement = screen.getByLabelText('span');
+        expect( spanElement.className ).toContain('text-decoration-line-through');
+
+    });
+
+    test('debe llamar el ToggleTodo cuando se hace click', () => {
+
+        render(
+            <TodoItem
+                todo={todo}
+                onDeleteTodo={onDeleteTodoMock}
+                onToggleTodo={onToggleTodoMock}
+            />
+        );
+        screen.debug();
+
+        const spanElement = screen.getByLabelText('span');
+        fireEvent.click( spanElement ); // Smilula el click sobre ese elemento
+        expect( onToggleTodoMock ).toHaveBeenCalledWith( todo.id ); //Se asegura de que se llama la función con el id de argumento
+
+    });
+
+    test('button debde llamar el deldeteTodo', () => {
+
+        render(
+            <TodoItem
+                todo={todo}
+                onDeleteTodo={onDeleteTodoMock}
+                onToggleTodo={onToggleTodoMock}
+            />
+        );
+        screen.debug();
+
+        const deleteButton = screen.getByRole('button');
+        fireEvent.click( deleteButton ); 
+        expect( onDeleteTodoMock ).toHaveBeenCalledWith( todo.id ); 
+        
+    });
+
 });
